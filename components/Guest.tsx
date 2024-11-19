10e1065b7f61efadb8b2dcb0d9d5bb53936c83d0
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2024-11-19 20:54:09 +0530

    finally clerk neondb prisma nextjs project

---
+++ components/Guest.tsx
@@ -0,0 +1,12 @@
+
+
+const Guest = async () => {
+  return (
+    <div className="guest">
+        <h1>Welcome</h1>
+        <p>Please sign in to manage your transactions</p>
+    </div>
+  )
+}
+
+export default Guest
