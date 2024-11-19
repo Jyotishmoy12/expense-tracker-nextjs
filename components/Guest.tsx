1d8c9012f1b93bfcb120c1f2eb1762e40d1d41ab
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2024-11-19 21:18:04 +0530

    basic setup done

--- components/Guest.tsx
+++ components/Guest.tsx
@@ -1,10 +1,11 @@
-
+import { SignInButton } from "@clerk/nextjs"
 
 const Guest = async () => {
   return (
     <div className="guest">
         <h1>Welcome</h1>
         <p>Please sign in to manage your transactions</p>
+        <SignInButton/>
     </div>
   )
 }
